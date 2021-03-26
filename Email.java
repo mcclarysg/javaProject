acd4b6dc0472f72dd91660f541d786ee7e47087f
mcclarysg <68408954+mcclarysg@users.noreply.github.com>
2021-03-26 00:32:48 -0400

    Add files via upload

--- Email.java
+++ Email.java
@@ -1,5 +1,6 @@
 package emailapp;
 
+import java.util.Scanner;
 public class Email {
     private String firstName;
     private String lastName;
@@ -14,8 +15,26 @@ public class Email {
         this.firstName=firstName;
         this.lastName=lastName;
         System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+
+        // call a method ask for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
     // ask for department
+    private String setDepartment(){
+        System.out.println("DEPARTMENT CODES\n1 for Sales \n2 for Development \n3 for Accounting\n0 for none\nEnter department code: " );
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "Sales"; }
+        else if (depChoice == 2) { return  "Dev"; }
+        else if (depChoice == 3) { return  "Acct"; }
+        else {return "";}
+
+
+
+
+    }
+
     // generate a random password
     // set mailbox capacity
     // set alt email
