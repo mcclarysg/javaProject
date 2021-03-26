81efaa124fb570d8133cedfc38624394fe9a2fa9
mcclarysg <68408954+mcclarysg@users.noreply.github.com>
2021-03-25 23:55:40 -0400

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,20 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternativeEmail;
+
+
+    //constructor to receive first name and last
+    // ask for department
+    // generate a random password
+    // set mailbox capacity
+    // set alt email
+    // change password
+
+
+}
---
+++ EmailApp.java
@@ -0,0 +1,9 @@
+package emailapp;
+public class EmailApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+}
